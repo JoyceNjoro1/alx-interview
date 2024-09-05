d3e93393fa881aa92c23efa449f730075e92c003
Joyce Njoroge <136052595+JoyceNjoro1@users.noreply.github.com>
2024-09-05 13:31:55 +0300

    primegame

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,58 +1,41 @@
 #!/usr/bin/python3
+"""Prime game module.
+"""
 
-def sieve_of_eratosthenes(n):
-    """ Generate a list of prime numbers up to n using the Sieve of Eratosthenes """
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False  # 0 and 1 are not primes
-    p = 2
-    while p * p <= n:
-        if primes[p]:
-            for i in range(p * p, n + 1, p):
-                primes[i] = False
-        p += 1
-    return primes
 
 def isWinner(x, nums):
-    """ Determines the winner of the prime game """
-    if not nums or x < 1:
+    """Determines the winner of a prime game session with `x` rounds."""
+    if x < 1 or not nums:
         return None
+
+    marias_wins, bens_wins = 0, 0
+    # Find the maximum number in nums to set sieve limit
+    n = max(nums)
     
-    # Find the maximum number in nums to optimize sieve generation
-    max_num = max(nums)
+    # Sieve of Eratosthenes to determine prime numbers up to n
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False  # 0 and 1 are not primes
     
-    # Get primes up to the largest number in the game using Sieve of Eratosthenes
-    primes = sieve_of_eratosthenes(max_num)
-    
-    # Track wins for Maria and Ben
-    maria_wins = 0
-    ben_wins = 0
+    for i in range(2, int(n**0.5) + 1):
+        if primes[i]:
+            for j in range(i * i, n + 1, i):
+                primes[j] = False
     
     # Simulate each round
-    for n in nums:
-        available_numbers = list(range(1, n + 1))
-        prime_moves = [i for i in range(2, n + 1) if primes[i]]
-        maria_turn = True
-        while prime_moves:
-            # Pick the first available prime
-            current_prime = prime_moves[0]
-            
-            # Remove this prime and its multiples from the list
-            available_numbers = [num for num in available_numbers if num % current_prime != 0]
-            prime_moves = [num for num in prime_moves if num % current_prime != 0]
-            
-            # Switch turns
-            maria_turn = not maria_turn
+    for num in nums:
+        # Count how many primes are there up to `num`
+        primes_count = sum(primes[0:num + 1])
         
-        # If it's Maria's turn and no primes are left, Ben won, else Maria won
-        if maria_turn:
-            ben_wins += 1
+        # Maria wins if the prime count is odd, Ben wins if it's even
+        if primes_count % 2 == 0:
+            bens_wins += 1
         else:
-            maria_wins += 1
-    
-    # Determine overall winner
-    if maria_wins > ben_wins:
+            marias_wins += 1
+
+    # Determine the overall winner
+    if marias_wins > bens_wins:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif bens_wins > marias_wins:
         return "Ben"
     else:
         return None
